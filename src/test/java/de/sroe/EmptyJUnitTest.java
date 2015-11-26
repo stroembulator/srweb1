caa81c6deb27276b5b698c55b52552fb0ecf3770
steff <stefan.roenisch@web.de>
2015-11-26 18:23:35 +0100

    test2

---
+++ src/test/java/de/sroe/EmptyJUnitTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.sroe;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author steff
+ */
+public class EmptyJUnitTest {
+    
+    public EmptyJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+     @Test
+     public void hello1() {}
+
+     @Test
+     public void hello2() {}
+
+     @Test
+     public void hello3() {
+        fail();
+     }
+}
